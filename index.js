70ae9725788b2fd1ca39d89c7b4671c106cbb91c
lMusicl <dmezhenskij@yandex.ru>
2024-06-07 15:26:56 +0300

    Fixed privacy btn

--- index.js
+++ index.js
@@ -20,9 +20,13 @@ const generateRandomUA = () => {
 }
 
 async function loginToVimeo(username, password) {
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=1280,800'],
+        executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
+        defaultViewport: {
+            width:1280,
+            height:800
+        }});
     console.log("Open browser");
-
     const page = await browser.newPage();
     console.log("Create new page");
 
@@ -44,15 +48,15 @@ async function loginToVimeo(username, password) {
         throw new Error('Couldn\'t connect to the site Vimeo.com');
     }
 
-    try {
-        console.log("Waiting for cookies");
-        await page.locator('#onetrust-accept-btn-handler').wait();
-        console.log("Accept cookie button visible");
-        await page.locator('#onetrust-accept-btn-handler').click();
-        console.log("Click cookie button");
-    } catch (e) {
-        console.log("Cookies are not confirmed");
-    }
+    // try {
+    //     console.log("Waiting for cookies");
+    //     await page.locator('#onetrust-accept-btn-handler').wait();
+    //     console.log("Accept cookie button visible");
+    //     await page.locator('#onetrust-accept-btn-handler').click();
+    //     console.log("Click cookie button");
+    // } catch (e) {
+    //     console.log("Cookies are not confirmed");
+    // }
 
     try {
         console.log("Attempt to connect to the login page")
@@ -86,20 +90,33 @@ async function getLastVideo(page) {
     try {
         await page.goto('https://vimeo.com/manage/folders/5053677');
         console.log('Visited page folder');
-        console.log("Waiting video list");
-        await page.locator('.video_manager__table_item:first-child').wait();
-        console.log("Video list visible");
     } catch (e) {
         throw new Error("Folder page visit is error");
     }
 
+    try {
+        console.log("Waiting video list");
+        await page.locator('table tbody tr:first-child').wait();
+        console.log("Video list visible");
+    } catch (e) {
+        await page.locator('#layout-toggle-button').click();
+    }
+
+    try {
+        console.log("Waiting video list");
+        await page.locator('table tbody tr:first-child').wait();
+        console.log("Video list visible");
+    } catch (e) {
+        console.log("Couldn\'t find video list");
+    }
+
     const lastVideo = await page.evaluate(() => {
-        const videoElement = document.querySelector('.video_manager__table_item:first-child');
+        const videoElement = document.querySelector('table tbody tr:first-child');
         console.log("Found first video item")
         return {
-            title: videoElement.querySelector('.table_cell__title_wrapper > a').innerText,
-            url: videoElement.querySelector('.table_cell__thumb-wrapper a').getAttribute('href'),
-            privacy: videoElement.querySelector('.video_manager__table_cell--privacy').innerText
+            title: videoElement.querySelector('table tbody tr:first-child td:nth-child(3) span').innerText,
+            // url: videoElement.querySelector('table tbody tr:first-child td:nth-child(2)').getAttribute('href'),
+            privacy: videoElement.querySelector('table tbody tr:first-child td:nth-child(4) span').innerText
         };
     });
     console.log("Get video item link, title and privacy")
@@ -112,13 +129,14 @@ async function getLastVideo(page) {
 
 async function changePrivacySettings(page, lastVideo, browser) {
     console.log('Privacy function started');
-    if (lastVideo.privacy === 'Hidden') {
+    if (lastVideo.privacy === 'Hide from Vimeo') {
         console.log("The privacy policy settings for this video are correct");
         await browser.close();
-        return false;
+        process.exit();
     }
 
-    await page.goto('https://vimeo.com/manage/videos/' + lastVideo.url);
+    // await page.goto('https://vimeo.com/manage/videos/' + lastVideo.url);
+    await page.locator('table tbody tr:first-child').click();
     console.log('Visited video setting page');
     await page.waitForNavigation();
 
@@ -147,31 +165,40 @@ async function changePrivacySettings(page, lastVideo, browser) {
         throw new Error("Error loading privacy settings");
     }
 
-    await page.locator('#privacy-link-option-disable').click();
+    await page.locator('.chakra-radio:nth-child(3)').click();
     console.log("Click Hide from Vimeo button");
 
-    await page.waitForNavigation();
+    try {
+        await page.waitForNavigation();
+    } catch (e) {
+        console.log('The privacy policy settings for video "' + lastVideo.title + '" have been successfully changed.');
+        await browser.close();
+        process.exit();
+    }
+
     console.log('The privacy policy settings for video "' + lastVideo.title + '" have been successfully changed.');
     await browser.close();
-    return false;
+    process.exit();
 }
 
 async function main() {
     try {
-        const {browser, page} = await loginToVimeo(process.env.MY_USERNAME, process.env.MY_PASSWORD);
+        var {browser, page} = await loginToVimeo("support@beautygymclub.de", "w.ZQe8:v4wB?QfY");
         const lastVideo = await getLastVideo(page);
         console.log("Logged video information");
         console.log('Last video:', lastVideo);
         await changePrivacySettings(page, lastVideo, browser);
+
     } catch (e) {
         ErrorsCount += 1;
         if (ErrorsCount > 4) {
             console.log(e.message);
             console.log("The number of attempts exceeded");
-            return false;
+            process.exit();
         } else {
             console.log("Something went wrong. Let's try again!");
             console.log("Try: " + ErrorsCount);
+            await browser.close();
             await main();
         }
     }
